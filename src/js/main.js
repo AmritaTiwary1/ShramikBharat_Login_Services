27d875d0d633418fd9df27793c6dbd93000e5951
sumit-munda <sumitkmunda.563@gmail.com>
2025-02-05 02:12:21 +0530

    Create a responsive header without animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
-// hamburger menu 
+// hamburger menu
 
-const nav = document.querySelector('.navbar')
-const hambtn = document.querySelector('.hamburger');
-hambtn.addEventListener('click', () => {
-  alert("hello")
-nav.classList.toggle('active');
-})
+const nav = document.querySelector(".navbar");
+const hambtn = document.querySelector(".hamburger");
+hambtn.addEventListener("click", () => {
+	nav.classList.add("active");
+});
 
+const crossbtn = document.querySelector(".cross");
+crossbtn.addEventListener("click", () => {
+	nav.classList.remove("active");
+});
 
 // /* Open register.html file on click of register button  */
 
-// const registerButton = document.querySelector('.register-btn'); 
+// const registerButton = document.querySelector('.register-btn');
 // registerButton.onclick = e => {
-//   window.location.href = './register.html'; 
+//   window.location.href = './register.html';
 // }

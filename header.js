013e8f6c1fe0544d60cd6dc29abea7a60ee118a5
sumit-munda <sumitkmunda.563@gmail.com>
2025-02-18 11:52:38 +0530

    Improved the contact, login and register page.

--- header.js
+++ header.js
@@ -1,33 +1,67 @@
-const headerHTML = `  <header class="section-navbar">
-      <div class="head container">
+const headerHTML = `  <header class="section-navbar container">
         <div class="web-brand">
-         
-          <h2 style="color: rgb(12, 12, 123);" class="brand-name">Shramik Bharat</h2>
+        <i class="hamburger fa-solid fa-burger"></i>
+          <a href="/">
+          <img
+            src="./assets/images/logo.svg"
+            alt="brand logo"
+            class="brand-logo"
+          />
+        </a>
         </div>
 
-        <navbar class="navbar">
+        <div class="navbar">
+        <navbar>
+        <i class="cross fa-solid fa-xmark"></i>
           <ul>
-            <li class="nav-link"><a href="/" class="nav-link">home</a></li>
+            <li class="nav-link">
+              <a href="/" class="nav-link">home</a>
+            </li>
             <li class="nav-link" id="services">
               <div class="menu">
-                <a href="service.html" class="nav-link"
-                  >services <i class="fa-solid"></i
+                <a href="#services" class="nav-link"
+                  >services <i class="fa-solid fa-chevron-down"></i
                 ></a>
+                <ul class="dropdown">
+                  <li class="services-menu"><a href="#">Labourer</a></li>
+                  <li class="services-menu"><a href="#">Plumber</a></li>
+                  <li class="services-menu"><a href="#">Electrician</a></li>
+                  <li class="services-menu"><a href="#">Welder</a></li>
+                  <li class="services-menu"><a href="#">Painter</a></li>
+                </ul>
               </div>
             </li>
-            <li class="nav-link">
-              <a href="/" class="nav-link">blogs</a>
+            <li class="nav-link" id="resources">
+              <div class="menu">
+                <a href="#resources" class="nav-link"
+                  >resources <i class="fa-solid fa-chevron-down"></i
+                ></a>
+                <ul class="dropdown">
+                  <li class="resources-menu">
+                    <a href="#why">Why Us ?</a>
+                  </li>
+                  <li class="resources-menu">
+                    <a href="#">Get Started</a>
+                  </li>
+                  <li class="resources-menu"><a href="#"></a></li>
+                  <li class="resources-menu"><a href="#"></a></li>
+                  <li class="resources-menu"><a href="#"></a></li>
+                </ul>
+              </div>
+            </li>
+           <li class="nav-link">
+              <a href="#blogs" class="nav-link">blogs</a>
             </li>
             <li class="nav-link">
-              <a href="contact.html" class="nav-link">contact us</a>
+              <a href="/contact.html" class="nav-link">contact us</a>
             </li>
           </ul>
         </navbar>
+        </div>
 
         <div class="account">
-          <a href="Login.html"><button class="login-btn">login</button></a>
-          
-         <a href="Register.html"><button class="register-btn">register</button></a> 
+          <a class="login-btn" href="/login.html">login</a>
+        <a class="register-btn" href="/register.html">register</a>
         </div>
       </div>
     </header>
